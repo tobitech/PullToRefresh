cb5d7107c6a557de3190067a6ff5686d2ee0d1ec
Oluwatobi Omotayo <tobitech@ymail.com>
2022-09-06 14:34:22 +0100

    Initial Commit

---
+++ PullToRefresh/PullToRefreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullToRefreshApp.swift
+//  PullToRefresh
+//
+//  Created by Oluwatobi Omotayo on 06/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct PullToRefreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
